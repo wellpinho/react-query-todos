5f0d58fe2f4ba744339bb0bc9dd8e5035201f3c3
wellpinho <wellpinho@outlook.com>
2023-10-28 14:34:35 -0300

    react query instance refact

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -1,7 +1,6 @@
+import { queryClient } from "@/service/queryClient";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
+import { QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
